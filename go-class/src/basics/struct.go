f74b9cc849b020aab45a0ad7bd5dbaf19b780201
Matthias Kerk <tuxlife@users.noreply.github.com>
2016-11-09 16:58:57 +0100

    Struct without Pointers

--- go-class/src/basics/struct.go
+++ go-class/src/basics/struct.go
@@ -8,9 +8,9 @@ type T struct {
 }
 
 func main() {
-	var t0 *T = &T{1, 2} // ptr to T
+	var t0 T = T{1, 2} // ptr to T
 	t0.b = 3
-	t1 := &T{3, 4} // type taken from expr
+	t1 := T{3, 4} // type taken from expr
 	t1.a = 2
 	fmt.Println("t1.a:", t1.a, ", t0.b:", t0.b)
 }
